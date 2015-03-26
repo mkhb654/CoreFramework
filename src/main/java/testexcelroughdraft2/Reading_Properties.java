67dfcceb288431fbb6357d50287bae36f515e823
Muntaz <mkhb654@gmail.com>
2015-03-26 12:30:19 -0400

    updated test roughdraft to read excel file

---
+++ src/main/java/testexcelroughdraft2/Reading_Properties.java
@@ -0,0 +1,7 @@
+package testexcelroughdraft2;
+
+/**
+ * Created by mkaleem on 3/26/15.
+ */
+public class Reading_Properties {
+}
---
+++ src/main/java/testexcelroughdraft2/Xls_Reader.java
@@ -0,0 +1,7 @@
+package testexcelroughdraft2;
+
+/**
+ * Created by mkaleem on 3/26/15.
+ */
+public class Xls_Reader {
+}
